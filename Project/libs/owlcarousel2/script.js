cf1a8178e27137b9af3978994bf9572175a027ad
hopsini <56691002+hopsini@users.noreply.github.com>
2019-11-28 12:55:35 +0200

    Add files via upload

---
+++ Project/libs/owlcarousel2/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:2
+            }
+        }
+    });
+});
